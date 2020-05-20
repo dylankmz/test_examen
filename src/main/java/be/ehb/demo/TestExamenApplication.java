6f9ebefb3777ba14d1f26594c157ad50444b44c9
Dylan Karapekmez <dylankarapekmez@icloud.com>
2020-05-20 14:15:48 +0200

    initial commit

---
+++ src/main/java/be/ehb/demo/TestExamenApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestExamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestExamenApplication.class, args);
+    }
+
+}
